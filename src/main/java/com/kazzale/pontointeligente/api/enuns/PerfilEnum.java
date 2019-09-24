41a0ce4eba34c6dc022985a3001d88b0c72d5a63
Daniel Cabral da Silva <daniel.silva@fh.com.br>
2019-09-24 17:09:51 -0300

    Adicionado classes novas

---
+++ src/main/java/com/kazzale/pontointeligente/api/enuns/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.kazzale.pontointeligente.api.enuns;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
---
+++ src/main/java/com/kazzale/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,13 @@
+package com.kazzale.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kazzale.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
